1018b8994e9b0390ac128cc9b0233ff6087d1198
chen <chena7831.com>
2021-02-23 21:40:55 +0800

    update

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -5,17 +5,27 @@
     </nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <home-recommend-view :recommends="recommends"></home-recommend-view>
+    <feature-view></feature-view>
+    <tab-control class="tab-control" :titles="['流行','新款','精选']"></tab-control>
+    <goods-list :goods="goods['pop'].list"></goods-list>
+
   </div>
 </template>
 
 <script>
 
-import NavBar from 'components/common/navbar/NavBar'
+// 导入子组件
 import HomeSwiper from './childComps/HomeSwiper'
 import HomeRecommendView from './childComps/HomeRecommendView'
+import FeatureView from './childComps/FeatureView'
+// 导入公共组件
+import NavBar from 'components/common/navbar/NavBar'
+import TabControl from 'components/content/tabControl/TabControl'
+import GoodsList from 'components/content/goods/GoodsList'
 
-
-import {getHomeMultidata} from 'network/home'
+//导入网络请求的api
+import {
+  getHomeMultidata,getHomeGoods} from 'network/home'
 
 
 
@@ -24,29 +34,71 @@ export default {
   components: {
     NavBar,
     HomeSwiper,
-    HomeRecommendView
+    HomeRecommendView,
+    FeatureView,
+    TabControl,
+    GoodsList
   },
   data() {
     return {
       banners: [],
-      recommends: []
+      recommends: [],
+      goods: {
+        'pop': {page: 0,list: []},
+        'new': {page: 0,list: []},
+        'sell': {page: 0,list: []}
+      }
     }
   },
   created(){
     //请求多个数据
-    getHomeMultidata().then(res => {
-      console.log(res);
-      this.banners = res.data.data.banner.list //请求完毕后数据会释放，需要用data将请求到的数据保存起来
-      this.recommends = res.data.data.recommend.list
-    })
-  }
+    this.getHomeMultidata()
+
+     // 请求商品数据
+    this.getHomeGoods('pop')
+    this.getHomeGoods('new')
+    this.getHomeGoods('sell')
+    
+    },
+   methods: {
+     getHomeMultidata() {
+        getHomeMultidata().then(res => {
+          this.banners = res.data.data.banner.list //请求完毕后数据会释放，需要用data将请求到的数据保存起来
+          this.recommends = res.data.data.recommend.list
+        })
+     },
+     getHomeGoods(type) {
+       const page = this.goods[type].page + 1
+       getHomeGoods(type,page).then(res => {
+         console.log(res.data.data.list);
+        
+        this.goods[type].list.push(...res.data.data.list) 
+        this.goods[type].page += 1
+       })
+     }
+   }
+    
 }
 </script>
 
-<style>
+<style> 
+  #home {
+    padding-top: 44px;
+  }
+
   .home-nav{
     background-color: var(--color-tint);
     color: #fff;
     font-size: 18px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 9;
+  }
+  .tab-control {
+    position: sticky;
+    top: 44px;
+    z-index: 9;
   }
 </style>
--- src/views/Home/childComps/FeatureView.vue
+++ src/views/Home/childComps/FeatureView.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-
+  <div class="feature">
+    <a href="https://act.mogujie.com/zzlx67">
+      <img src="~assets/img/home/recommend_bg.jpg" alt="">
+    </a>
   </div>
 </template>
 
@@ -12,5 +14,7 @@ export default {
 </script>
 
 <style>
-
+  .feature img {
+    width: 100%;
+  }
 </style>
